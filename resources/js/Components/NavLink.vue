2e19f3363dbd5d0869811a1dbc4be4efe72b8756
tod2a <t.copin@yahoo.fr>
2025-01-14 11:39:27 +0100

    put the login and register page in french

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'inline-flex items-center rounded-md border border-transparent bg-green-200 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-secondary-dark transition duration-150 ease-in-out hover:bg-green-100 focus:bg-green-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-green-400'
+        ? 'inline-flex items-center rounded-md border border-transparent border-indigo-500 bg-green-200 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-secondary transition duration-150 ease-in-out hover:bg-green-100 focus:bg-green-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-green-400'
         : 'inline-flex items-center rounded-md border border-transparent bg-green-200 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-secondary transition duration-150 ease-in-out hover:bg-green-100 focus:bg-green-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-green-300',
 );
 </script>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -26,7 +26,7 @@ const submit = () => {
 
         <form @submit.prevent="submit" class="bg-white shadow-md rounded-lg p-6 mx-auto max-w-sm mt-12">
             <div>
-                <InputLabel for="name" value="Name" />
+                <InputLabel for="name" value="Nom" />
 
                 <TextInput
                     id="name"
@@ -57,7 +57,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password" value="Mot de passe" />
 
                 <TextInput
                     id="password"
@@ -74,7 +74,7 @@ const submit = () => {
             <div class="mt-4">
                 <InputLabel
                     for="password_confirmation"
-                    value="Confirm Password"
+                    value="Confirmer le mot de passe"
                 />
 
                 <TextInput
@@ -97,7 +97,7 @@ const submit = () => {
                     :href="route('login')"
                     class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                    Already registered?
+                    Déjà inscrit?
                 </Link>
 
                 <PrimaryButton
@@ -105,7 +105,7 @@ const submit = () => {
                     :class="{ 'opacity-25': form.processing }"
                     :disabled="form.processing"
                 >
-                    Register
+                    Inscription
                 </PrimaryButton>
             </div>
         </form>
