18a962fa0271185c3b860dc8d71bc2f08183f03c
tod2a <t.copin@yahoo.fr>
2025-05-01 18:34:49 +0200

    setup the dashboard vue and the sidebar, have to fix the layout

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+</script>
+
+<template>
+    <Head title="Admin Dashboard" />
+
+    <AuthenticatedLayout>
+        <p>Welcome to your dashboard</p>
+    </AuthenticatedLayout>
+</template>
