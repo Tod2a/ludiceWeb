860c36b782555d2ba5c930864d282a30801cd43d
tod2a <t.copin@yahoo.fr>
2025-05-06 23:59:46 +0200

    deployment setup on server

--- deploy.php
+++ deploy.php
@@ -3,10 +3,12 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
+require 'contrib/npm.php';
 
 // Config
 
-set('repository', 'https://github.com/Tod2a/ludiceWeb.git');
+set('repository', 'git@github.com:Tod2a/ludiceWeb.git');
+set('ssh_multiplexing', false);
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -18,6 +20,23 @@ host('tst.ludice.app')
     ->set('remote_user', 'ludice')
     ->set('deploy_path', '/data/ludice/app');
 
+// Tasks
+
+// Define custom tasks similar to npm:install for running `npm run build` command
+
+set('bin/npm', function () {
+    return which('npm');
+});
+
+task('npm:run_build', function () {
+    run("cd {{release_path}} && {{bin/npm}} run build");
+});
+
+
 // Hooks
+
+after('artisan:migrate', 'npm:install');
+after('npm:install', 'npm:run_build');
+
 
 after('deploy:failed', 'deploy:unlock');
