bfa17b877a69523dd83fd0307f751ff2ce944762
tod2a <t.copin@yahoo.fr>
2025-05-22 11:43:39 +0200

    adding more user for beta testers

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -26,9 +26,69 @@ class UserSeeder extends Seeder
             'role_id' => 2,
         ]);
 
+        $adminUser = User::factory()->create([
+            'name' => 'Admin2 User',
+            'email' => 'admin2@example.com',
+            'role_id' => 2,
+        ]);
+
+        $adminUser = User::factory()->create([
+            'name' => 'Admin3 User',
+            'email' => 'admin3@example.com',
+            'role_id' => 2,
+        ]);
+
         $regularUser = User::factory()->create([
-            'name' => 'User',
-            'email' => 'user@example.com',
+            'name' => 'User1',
+            'email' => 'user1@example.com',
+            'role_id' => 3,
+        ]);
+
+        $regularUser = User::factory()->create([
+            'name' => 'User2',
+            'email' => 'user2@example.com',
+            'role_id' => 3,
+        ]);
+
+        $regularUser = User::factory()->create([
+            'name' => 'User3',
+            'email' => 'user3@example.com',
+            'role_id' => 3,
+        ]);
+
+        $regularUser = User::factory()->create([
+            'name' => 'User4',
+            'email' => 'user4@example.com',
+            'role_id' => 3,
+        ]);
+
+        $regularUser = User::factory()->create([
+            'name' => 'User5',
+            'email' => 'user5@example.com',
+            'role_id' => 3,
+        ]);
+
+        $regularUser = User::factory()->create([
+            'name' => 'User6',
+            'email' => 'user6@example.com',
+            'role_id' => 3,
+        ]);
+
+        $regularUser = User::factory()->create([
+            'name' => 'User7',
+            'email' => 'user7@example.com',
+            'role_id' => 3,
+        ]);
+
+        $regularUser = User::factory()->create([
+            'name' => 'User8',
+            'email' => 'user8@example.com',
+            'role_id' => 3,
+        ]);
+
+        $regularUser = User::factory()->create([
+            'name' => 'User9',
+            'email' => 'user9@example.com',
             'role_id' => 3,
         ]);
 
