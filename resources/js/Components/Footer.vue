d68a52a81691ddfb3d906c9abeedb89c4fa0bb77
tod2a <t.copin@yahoo.fr>
2025-05-22 14:07:30 +0200

    adding the privacy policy

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -5,9 +5,9 @@
         <p>
             Contact : <a href="mailto:support@ludice.app" class="text-blue-500 hover:underline">support@ludice.app</a>
         </p>
-        <!-- <p class="mt-1">
-            <a href="/legal-notice" class="hover:underline">Mentions légales</a> |
-            <a href="/privacy-policy" class="hover:underline">Politique de confidentialité</a>
-        </p> -->
+        <p class="mt-1">
+            <!-- <a href="/legal-notice" class="hover:underline">Mentions légales</a> | -->
+            <a :href="route('privacy')" class="hover:underline">Politique de confidentialité</a>
+        </p>
     </footer>
 </template>
