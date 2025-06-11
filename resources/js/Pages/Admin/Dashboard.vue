5968693c85bcddc0838b4b403fe19b8c17b2404f
tod2a <t.copin@yahoo.fr>
2025-06-11 14:55:33 +0200

    review modals

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { Head, useForm } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { FwbModal } from 'flowbite-vue';
 import { ref } from 'vue';
 import Pagination from '@/Components/Pagination.vue';
+import Modal from '@/Components/Modal.vue';
 
 const props = defineProps({
     appStartWeek: Number,
@@ -120,33 +120,33 @@ const closePatchModal = () => {
 
     </AuthenticatedLayout>
 
-    <fwb-modal size="md" position="top-center" v-if="isPatchModalVisible" @close="closePatchModal">
-        <template #header>
-            <h2 class="text-lg font-medium text-gray-200">
-                Ajoutez une nouvelle note.
-            </h2>
+    <Modal :show="isPatchModalVisible" @close="closePatchModal">
+        <template #title>
+            <h2 class="text-lg font-medium text-gray-900">Ajoutez une nouvelle note.</h2>
         </template>
+
         <template #body>
-            <div>
-                <label for="title" class="block text-sm font-medium text-gray-200">Titre</label>
+            <div class="mb-4">
+                <label for="title" class="block text-sm font-medium text-gray-700">Titre</label>
                 <input id="title" type="text" v-model="form.title" required maxlength="255"
                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring focus:ring-green-300 focus:ring-opacity-50" />
             </div>
             <div>
-                <label for="description" class="block text-sm font-medium text-gray-200">Description</label>
+                <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
                 <textarea id="description" v-model="form.description" required rows="4"
                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring focus:ring-green-300 focus:ring-opacity-50"></textarea>
             </div>
         </template>
+
         <template #footer>
             <button @click="closePatchModal"
-                class="text-gray-500 mr-2 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5">
+                class="text-gray-600 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-300 text-sm font-medium px-5 py-2.5">
                 Annuler
             </button>
             <button @click="postPatchNote"
-                class="text-white bg-green-800 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">
+                class="text-white bg-green-800 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5">
                 Ajouter
             </button>
         </template>
-    </fwb-modal>
+    </Modal>
 </template>
